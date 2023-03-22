974f09dea51060ea94531de45decaed388ecba62
kc-dipendra <dkc.pkr@gmail.com>
2023-03-22 16:06:14 -0400

    updated

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@
 import os
 import sys
 
-
-def main():
+#def main():
+if __name__ == '__main__':
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentdb.settings')
     try:
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
